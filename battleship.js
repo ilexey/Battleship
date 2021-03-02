5537b52f7eb05646f72b058c8ecb1356095f158c
ilexey <alexeyshitenkov@yandex.ru>
2021-03-02 23:15:31 +0300

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -12,3 +12,39 @@ let view = {
         cell.setAttribute("class", "hit");
     },
 };
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [{ locations: ["06", "16", "26"], hits: ["","",""] },
+            { locations: ["24", "34", "44"], hits: ["","",""] },
+            { locations: ["10", "11", "12"], hits: ["","",""] }],
+    fire: function(guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    this.shipsSunk++;
+                    view.displayMessage("You sunk my battleship!");
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function(ship) {
+        for (i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+}
